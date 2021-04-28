3100ce2bc13de9195f24cfe7905adb8ce2fbee60
David Pierron <dpierron@gmail.com>
2021-04-28 09:20:10 +0200

    corrected paths

--- routes/privateSlmod.js
+++ routes/privateSlmod.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET private server SLMOD json data pages. */
 router.get('/stats', function(req, res, next) {
-  res.sendFile('/Users/veaf/Saved Games/private_server_2.5.6/Slmod/SlmodStats.json', { root: '' });
+  res.sendFile('/Users/veaf/Saved Games/private_server/Slmod/SlmodStats.json', { root: '' });
 });
 
 router.get('/metastats', function(req, res, next) {
-  res.sendFile('/Users/veaf/Saved Games/private_server_2.5.6/Slmod/SlmodMetaStats.json', { root: '' });
+  res.sendFile('/Users/veaf/Saved Games/private_server/Slmod/SlmodMetaStats.json', { root: '' });
 });
 
 router.get('/penaltystats', function(req, res, next) {
-  res.sendFile('/Users/veaf/Saved Games/private_server_2.5.6/Slmod/SlmodPenaltyStats.json', { root: '' });
+  res.sendFile('/Users/veaf/Saved Games/private_server/Slmod/SlmodPenaltyStats.json', { root: '' });
 });
 
 module.exports = router;
--- routes/publicSlmod.js
+++ routes/publicSlmod.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET public server SLMOD json data pages. */
 router.get('/stats', function(req, res, next) {
-  res.sendFile('/Users/veaf/Saved Games/public_server_2.5.6/Slmod/SlmodStats.json', { root: '' });
+  res.sendFile('/Users/veaf/Saved Games/public_server/Slmod/SlmodStats.json', { root: '' });
 });
 
 router.get('/metastats', function(req, res, next) {
-  res.sendFile('/Users/veaf/Saved Games/public_server_2.5.6/Slmod/SlmodMetaStats.json', { root: '' });
+  res.sendFile('/Users/veaf/Saved Games/public_server/Slmod/SlmodMetaStats.json', { root: '' });
 });
 
 router.get('/penaltystats', function(req, res, next) {
-  res.sendFile('/Users/veaf/Saved Games/public_server_2.5.6/Slmod/SlmodPenaltyStats.json', { root: '' });
+  res.sendFile('/Users/veaf/Saved Games/public_server/Slmod/SlmodPenaltyStats.json', { root: '' });
 });
 
 module.exports = router;
